8db4ebaf769b3ca7585642fdd8d9fe055e348e01
suzeng <zuotianyumingtian@yunjinghuchuang.com>
2023-04-27 16:51:36 +0800

    更新

--- src/utils/getEnv.ts
+++ src/utils/getEnv.ts
@@ -1,7 +1,6 @@
 // Read all environment variable configuration files to process.env
 export function wrapperEnv(envConf: Recordable): ViteEnv {
 	const ret: any = {};
-
 	for (const envName of Object.keys(envConf)) {
 		let realName = envConf[envName].replace(/\\n/g, "\n");
 		realName = realName === "true" ? true : realName === "false" ? false : realName;
