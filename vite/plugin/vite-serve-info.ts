7da75a6aa28db46dba8bbe4260d1c9adfc6bb0c6
郭某人 <129492349+Yolo-00@users.noreply.github.com>
2024-03-15 17:18:50 +0800

    fix: 修改vite-serve-info插件

--- vite/plugin/vite-serve-info.ts
+++ vite/plugin/vite-serve-info.ts
@@ -1,8 +1,9 @@
 import { Plugin } from "vite";
-// import { printLog } from "../../src/utils/index";
-// import { bold, green, magenta } from "picocolors";
-// import dayjs from "dayjs";
-// import pack from "../../package.json";
+import { printLog } from "../../src/utils/index";
+import gradientString from "gradient-string";
+import dayjs from "dayjs";
+import pack from "../../package.json";
+
 /**
  * viteServeInfo插件
  */
@@ -10,9 +11,9 @@ export function viteServeInfoPlugin(): Plugin {
 	return {
 		name: "vite:serve-info",
 		buildStart() {
-			// console.log(bold(printLog()));
-			// console.log(bold(green(`当前时间 ${magenta(dayjs().format("YYYY年MM月DD日 HH时mm分ss秒"))}`)));
-			// console.log(bold(green(`欢迎使用 ${magenta(pack.name)} ${magenta("v:" + pack.version)}`)));
+			console.log(gradientString("cyan", "pink", "magenta")(printLog()));
+			console.log(gradientString("cyan", "pink", "magenta")(`当前时间 ${dayjs().format("YYYY年MM月DD日 HH时mm分ss秒")}`));
+			console.log(gradientString("cyan", "pink", "magenta")(`欢迎使用 ${pack.name} ${"v:" + pack.version}`));
 		}
 	};
 }
