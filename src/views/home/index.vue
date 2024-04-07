82a39685e0c6fbf231ff2651576b2bb1323feeae
郭某人 <129492349+Yolo-00@users.noreply.github.com>
2024-04-07 18:05:47 +0800

    fix: update

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,19 @@
 <template>
-	<div style="height: 1000px">home</div>
+	<div>
+		<!-- <div style="height: 1000px">home</div> -->
+		<el-button type="primary" @click="handle">按钮</el-button>
+		<PDFPreview ref="pdfPreviewRef" />
+	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PDFPreview from "@/components/PDFPreview/index.vue";
+import { ref } from "vue";
+
+const pdfPreviewRef = ref();
+const handle = () => {
+	pdfPreviewRef.value.open();
+};
+</script>
 
 <style scoped lang="scss"></style>
