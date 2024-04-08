b35ddd71b105fb32b592c81e7095d8cb0529b7de
郭某人 <129492349+Yolo-00@users.noreply.github.com>
2024-04-08 17:06:49 +0800

    fix: update

--- src/components/QRCode/index.vue
+++ src/components/QRCode/index.vue
@@ -3,15 +3,14 @@ import { ref, onMounted, computed } from "vue";
 import QRCode, { type QRCodeRenderersOptions } from "qrcode";
 import { generateUUID } from "@/utils/util";
 
-// img 支持预览不支持logo,canvas支持logo不支持预览
 interface QRCodeProps {
-	type?: "canvas" | "img"; // 二维码类型 ==> 默认img
+	type?: "canvas" | "img"; // 二维码类型 ==> 默认img, img 支持预览,canvas支持logo
 	size?: number; // 二维码大小 ==> 默认200
-	iconSize?: number; // 二维码图标大小 ==> 默认50
+	iconSize?: number; // 二维码图标大小 ==> 默认40
 	content: string; // 二维码内容 ==> 必填
 	logo?: string; // 二维码logo ==> 默认无
 	options?: QRCodeRenderersOptions; // 二维码配置 ==> 默认无
-	errorLevel?: "L" | "M" | "Q" | "H"; // 二维码容错级别 ==> 默认M
+	errorLevel?: "L" | "M" | "Q" | "H"; // 二维码容错级别 ==> 默认H
 }
 
 // 接收父组件参数并设置默认值
