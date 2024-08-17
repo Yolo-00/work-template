6facce4dfef2e839472565769fbd403db6a84b84
Yolo-00 <Yolo-00@users.noreply.github.com>
2024-08-17 10:42:02 +0000

    deploy: ea403e570a8f92ffc2a30a5d6a52f657e17cfa70

---
+++ assets/js/404-DXfCAKTP.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-CNxpSwrv.js";import{g as r,i as c}from"./vendor-C7EFnCic.js";const o={};function t(n,_){return c(),r("div",null,"404")}const f=e(o,[["render",t]]);export{f as default};
---
+++ assets/js/useLanguage-CYYjU2cq.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-CNxpSwrv.js";import{P as u}from"./vendor-C7EFnCic.js";function g(){const a=t(),{locale:o}=u();return{setLanguage:e=>{e!=a.language&&(a.setLanguage(e),o.value=e)}}}export{g as u};
