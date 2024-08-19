bf178e8e923d5babb1028deb84333568c738335a
Yolo-00 <Yolo-00@users.noreply.github.com>
2024-08-19 10:15:06 +0000

    deploy: 6f0564c5733af4c983fc59986deb9dce009abccd

--- assets/js/404-AkolyFiX.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-BCy7sfuz.js";import{h as r,j as c}from"./vendor-yV4zkE9G.js";const o={};function t(n,_){return c(),r("div",null,"404")}const f=e(o,[["render",t]]);export{f as default};
