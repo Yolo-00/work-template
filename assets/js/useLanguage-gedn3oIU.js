501418296023117a1b9e200127579a9824eceecb
Yolo-00 <Yolo-00@users.noreply.github.com>
2024-08-19 03:43:03 +0000

    deploy: 7f7aa59153ce1cc52f6eafbe336b112d908546be

---
+++ assets/js/useLanguage-gedn3oIU.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-BCy7sfuz.js";import{Q as u}from"./vendor-yV4zkE9G.js";function g(){const a=t(),{locale:o}=u();return{setLanguage:e=>{e!=a.language&&(a.setLanguage(e),o.value=e)}}}export{g as u};
