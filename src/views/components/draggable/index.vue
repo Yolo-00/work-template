5213201529863e4139e47f02aae404c0f6e04d03
Yolo <g20180630105@gmail.com>
2024-10-12 11:52:56 +0800

    feat: 增加移动端拖拽排序功能

--- src/views/components/draggable/index.vue
+++ src/views/components/draggable/index.vue
@@ -5,28 +5,98 @@ import { ref } from "vue";
 const list = ref([
 	{
 		id: 1,
-		name: "John"
+		name: "张三"
 	},
 	{
 		id: 2,
-		name: "Jane"
+		name: "李四"
 	},
 	{
 		id: 3,
-		name: "Joe"
+		name: "王五"
+	},
+	{
+		id: 3,
+		name: "aaa"
 	}
 ]);
+const dragging = ref(false);
+const dragIndex = ref(0);
+const dragData = ref("");
+const clientX = ref(0);
+const clientY = ref(0);
+const draggingStyle = ref({});
+const onTouchStart = (index: number, e: any) => {
+	// console.log(index, e, "开始");
+	dragIndex.value = index;
+	dragData.value = list.value[index].name;
+	draggingStyle.value = {
+		left: `${e.touches[0].clientX - 40}px`,
+		top: `${e.touches[0].clientY - 40}px`,
+		opacity: 0.5
+	};
+	dragging.value = true;
+};
+const onTouchMove = (e: any) => {
+	// console.log(e, "进行中");
+	if (dragging.value) {
+		draggingStyle.value = {
+			left: `${e.touches[0].clientX - 40}px`,
+			top: `${e.touches[0].clientY - 40}px`,
+			opacity: 0.5
+		};
+	}
+	clientX.value = e.touches[0].clientX;
+	clientY.value = e.touches[0].clientY;
+};
+const onTouchEnd = () => {
+	console.log(clientX.value, clientY.value, "结束");
+	const elList = document.querySelectorAll(".grid-item");
+	elList.forEach((el, index: number) => {
+		const itemRect = el.getBoundingClientRect();
+		console.log(itemRect);
+		if (
+			clientX.value > itemRect.left &&
+			clientX.value < itemRect.left + itemRect.width &&
+			clientY.value > itemRect.top &&
+			clientY.value < itemRect.top + itemRect.height
+		) {
+			console.log(el, itemRect, "命中");
+			if (dragging.value) {
+				list.value[dragIndex.value].name = list.value[index].name;
+				list.value[index].name = dragData.value;
+			}
+		}
+	});
+	dragging.value = false;
+	draggingStyle.value = {};
+};
 </script>
 
 <template>
 	<div>
-		<draggable v-model="list" item-key="id">
+		<draggable v-model="list" item-key="id" v-if="true">
 			<template #item="{ element }">
 				<el-card class="mt-5">
 					<div>{{ element.name }}</div>
 				</el-card>
 			</template>
 		</draggable>
+		<div class="w-80 grid grid-cols-3 bg-gray-300 p-5 gap-5" v-else>
+			<div
+				v-for="(item, index) in list"
+				:key="item.id"
+				class="w-20 h-20 bg-white flex justify-center items-center grid-item"
+				@touchstart="onTouchStart(index, $event)"
+				@touchmove="onTouchMove($event)"
+				@touchend="onTouchEnd()"
+			>
+				{{ item.name }}
+			</div>
+			<div v-if="dragging" class="w-20 h-20 bg-white flex justify-center items-center fixed" :style="draggingStyle">
+				{{ dragData }}
+			</div>
+		</div>
 	</div>
 </template>
 
---
+++ src/views/home/JsxComponent.tsx
@@ -0,0 +1,34 @@
+import { defineComponent, ref, onMounted } from "vue";
+
+const JsxComponent = defineComponent({
+	name: "JsxComponent",
+	setup() {
+		const value = ref(0);
+		const value2 = ref("");
+
+		onMounted(() => {
+			console.log("挂载");
+		});
+		return () => (
+			<div>
+				<ul>
+					{Array(5)
+						.fill(null)
+						.map((item, index) => (
+							<li key={index}>{index + 1}</li>
+						))}
+					<li>11</li>
+				</ul>
+				<el-button type="primary" onClick={() => value.value++}>
+					{value.value}
+				</el-button>
+				{value.value > 5 ? <div>大于五了</div> : <div>小于五</div>}
+				{value.value > 5 && <div>大于五了-----</div>}
+				{value2.value}
+				<el-input v-model={value2.value} placeholder="请输入内容" />
+			</div>
+		);
+	}
+});
+
+export default JsxComponent;
