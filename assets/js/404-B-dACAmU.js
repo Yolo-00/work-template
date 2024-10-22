099e7c5eaf45c41ec3cbcb28db7e7f776048527f
Yolo-00 <Yolo-00@users.noreply.github.com>
2024-10-22 10:31:27 +0000

    deploy: 9c953c38146e905afd48fd46447c34b44f2f2e9a

---
+++ assets/js/404-B-dACAmU.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-C1ILFQut.js";import{h as r,j as c}from"./vendor-tduSOmKd.js";const o={};function t(n,_){return c(),r("div",null,"404")}const f=e(o,[["render",t]]);export{f as default};
--- assets/js/useLanguage-O7HSC4SP.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./index-DwWdx6EJ.js";import{a6 as u}from"./vendor-tduSOmKd.js";function g(){const a=t(),{locale:o}=u();return{setLanguage:e=>{e!=a.language&&(a.setLanguage(e),o.value=e)}}}export{g as u};
