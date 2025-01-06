714bc6565af2fa2afffe89a0cdb83bae4ad47a21
Yolo-00 <Yolo-00@users.noreply.github.com>
2025-01-06 09:11:45 +0000

    deploy: 097615bdc7400bf627d965d7878d8315cae6e7c4

--- assets/js/useLanguage-Dns_EimB.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./index-BYT9a_t7.js";import{a7 as u}from"./vendor-DH3m-RmB.js";function g(){const a=t(),{locale:o}=u();return{setLanguage:e=>{e!=a.language&&(a.setLanguage(e),o.value=e)}}}export{g as u};
