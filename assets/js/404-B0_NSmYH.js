5871ce3ebe4cc0673442f7bc88927bdabf1b5dfc
Yolo-00 <Yolo-00@users.noreply.github.com>
2025-03-24 09:09:28 +0000

    deploy: 9298d2fee70c86c21b9cfb5d887140910405ba43

--- assets/js/404-B0_NSmYH.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-Qkz2es8W.js";import{h as r,j as c}from"./vendor-BAJA0oMh.js";const o={};function t(n,_){return c(),r("div",null,"404")}const f=e(o,[["render",t]]);export{f as default};
