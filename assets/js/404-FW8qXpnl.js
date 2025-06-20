88ae44d6fe29621e0add3a843c4fddbdd708a288
Yolo-00 <Yolo-00@users.noreply.github.com>
2025-06-20 03:27:37 +0000

    deploy: e01137408eae46822ceb2d83751e1a6894835472

---
+++ assets/js/404-FW8qXpnl.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-C2-ClZ9e.js";import{h as r,j as c}from"./vendor-9Av3Cne1.js";const o={};function t(n,_){return c(),r("div",null,"404")}const f=e(o,[["render",t]]);export{f as default};
---
+++ assets/js/index-ARHq9Zct.js
@@ -0,0 +1,1 @@
+import t from"./index-DHK3WGW-.js";import{e as o,j as a,h as r,i as n,w as l,v as s}from"./vendor-9Av3Cne1.js";import"./index-C2-ClZ9e.js";const c=o({__name:"index",setup(d){return(i,e)=>(a(),r("div",null,[n(t,null,{default:l(()=>e[0]||(e[0]=[s("h1",null,"hello world",-1)])),_:1})]))}});export{c as default};
