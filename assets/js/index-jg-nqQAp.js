9f79c35af5fc74805242beb25c65894a5ba307ac
Yolo-00 <Yolo-00@users.noreply.github.com>
2025-09-24 03:33:44 +0000

    deploy: 1a4379928cd07cc53a70fc703f6308b8784ce2f7

--- assets/js/index-jg-nqQAp.js
+++
@@ -1,1 +0,0 @@
-import t from"./index-CAKId65z.js";import{e as o,j as a,h as r,i as n,w as l,v as s}from"./vendor-DzbK7Vmk.js";import"./index-CEG1FlkB.js";const c=o({__name:"index",setup(d){return(i,e)=>(a(),r("div",null,[n(t,null,{default:l(()=>e[0]||(e[0]=[s("h1",null,"hello world",-1)])),_:1})]))}});export{c as default};
---
+++ assets/js/useLanguage-Hdydkiou.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-Bt1GyEh0.js";import{Q as u}from"./vendor-DzbK7Vmk.js";function g(){const a=t(),{locale:o}=u();return{setLanguage:e=>{e!=a.language&&(a.setLanguage(e),o.value=e)}}}export{g as u};
